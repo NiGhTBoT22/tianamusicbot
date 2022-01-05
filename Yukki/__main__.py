e6c303f0e12e1a93c790ca5ecee1a0ce05b3aeb9
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-01-05 19:25:34 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -240,12 +240,12 @@ async def shikhar(_, CallbackQuery):
 
 @app.on_callback_query(filters.regex(r"help_(.*?)"))
 async def help_button(client, query):
-    home_match = re.match(r"help_home\((.+?)\)", query.data)
-    mod_match = re.match(r"help_module\((.+?)\)", query.data)
-    prev_match = re.match(r"help_prev\((.+?)\)", query.data)
-    next_match = re.match(r"help_next\((.+?)\)", query.data)
-    back_match = re.match(r"help_back", query.data)
-    create_match = re.match(r"help_create", query.data)
+    home_match = re.match(r"help_homes\((.+?)\)", query.data)
+    mod_match = re.match(r"help_modules\((.+?)\)", query.data)
+    prev_match = re.match(r"help_prevs\((.+?)\)", query.data)
+    next_match = re.match(r"help_nexts\((.+?)\)", query.data)
+    back_match = re.match(r"help_backs", query.data)
+    create_match = re.match(r"help_creates", query.data)
     top_text = f"""Hello {query.from_user.first_name},
 
 Click on the buttons for more information.
@@ -264,7 +264,7 @@ All commands can be used with: /
             [
                 [
                     InlineKeyboardButton(
-                        text="↪️ Back", callback_data="help_back"
+                        text="↪️ Back", callback_data="help_backs"
                     ),
                     InlineKeyboardButton(
                         text="🔄 Close", callback_data="close"
